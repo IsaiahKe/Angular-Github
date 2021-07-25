29c347e9c88cbb55126e14f43567df918b4380ec
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-25 17:32:58 +0300

    Add material modules file

---
+++ src/app/material.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import{MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar'
+@NgModule({
+imports:[
+    MatCardModule,
+    MatToolbarModule
+],
+exports:[
+    MatCardModule,
+    MatToolbarModule
+]
+})
+export class Material {}
