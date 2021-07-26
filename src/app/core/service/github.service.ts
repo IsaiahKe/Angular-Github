53095da3090c63ca9b7e5ea3e8ba99f2ae90e08e
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-26 15:56:59 +0300

    Update services

--- src/app/core/service/github.service.ts
+++ src/app/core/service/github.service.ts
@@ -3,24 +3,21 @@ import {HttpClient} from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import 'rxjs/add/operator/map';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
-  private username:string;
+  public username:string;
   constructor(private http:HttpClient) {
-    this.username= "IsaiahKe";
+    this.username='isaiahke'
    }
- /* getData():Observable<IUser>{
-    return this.http.get<IUser>(environment.path);
-  }
-  getUser(username:string):Observable<any>{
-    return this.http.get('');
-  }*/
+   
   getProfile(){
     return this.http.get(environment.path+this.username);
   }
   getRepos(){
     return this.http.get(environment.path +this.username+'/repos');
   }
+  
 }
