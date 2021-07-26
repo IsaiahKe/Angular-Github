4c80c9208c293cd5b601b3da4cf59ac9f00c66b2
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-26 15:51:00 +0300

    Update search content

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/core/model/user';
 import { GithubService } from 'src/app/core/service/github.service';
 
 @Component({
@@ -8,12 +7,9 @@ import { GithubService } from 'src/app/core/service/github.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-users:User[]=[];
+
   constructor(private service:GithubService) { }
-// getUser(){
-//   this.service.getProfile().subscribe((data)=>{(console.log(data));})
-//   //console.log(this.users);
-// }
+
   ngOnInit(): void {
   }
 
