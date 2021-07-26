27f0107583ea1a3c8d7f2bd922b2fe15ed86f83c
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-26 15:58:05 +0300

    Add material icon and buttons

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,14 +1,20 @@
 import { NgModule } from "@angular/core";
 import{MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{MatButtonModule} from '@angular/material/button';
+import{MatIconModule} from '@angular/material/icon'
 @NgModule({
 imports:[
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
 ],
 exports:[
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
 ]
 })
 export class Material {}
