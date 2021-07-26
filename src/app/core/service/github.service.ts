75fdd6ad8e9f3baf2fac0079f0a0dd8561203e23
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-26 23:53:28 +0300

    update all requirements

--- src/app/core/service/github.service.ts
+++ src/app/core/service/github.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import 'rxjs/add/operator/map';
+import { User } from '../model/user';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  user!:User;
   public username:string;
   constructor(private http:HttpClient) {
     this.username='isaiahke'
    }
-   
+
+
   getProfile(){
     return this.http.get(environment.path+this.username);
   }
